4f344d73a0ae190abcbef6f9a1f7c1afaac64955
Emil Steiner <emil.arnold.steiner@gmail.com>
2026-06-12 15:14:17 +0200

    Fixed Routing issue to Backup server

--- config_dmz.sh
+++ config_dmz.sh
@@ -2,6 +2,10 @@
 
 # Routing
 ip route add default via 10.20.30.1
+ip route add 10.20.31.0/24 via 10.20.30.2
+ip route add 10.20.32.0/24 via 10.20.30.2
+ip route add 10.20.33.0/24 via 10.20.30.2
+
 
 # DNS
 tee /etc/dnsmasq.conf 1>/dev/null << 'EOF'
