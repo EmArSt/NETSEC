badb0184b4eadd28ae633142a31c69cbc6605a9f
Emil Steiner <emil.arnold.steiner@gmail.com>
2026-06-12 15:35:14 +0200

    Backup inits before DMZ

--- run_configs.sh
+++ run_configs.sh
@@ -2,10 +2,9 @@
 
 ip netns exec Router1 ./config_router1.sh
 ip netns exec Router2 ./config_router2.sh
+ip netns exec Backup ./config_backup.sh
 ip netns exec DMZ ./config_dmz.sh
 ip netns exec Client1 ./config_client1.sh
-ip netns exec Backup ./config_backup.sh
-
 
 
 ./mwg.sh
